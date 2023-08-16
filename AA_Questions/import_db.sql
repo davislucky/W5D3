00609b4c59a4d19a7e443158b6b96e855350ec8e
swathi <swathibalasubramanyam@gmail.com>
2023-08-16 13:56:06 -0700

    fixed the import_db_sql

--- AA_Questions/import_db.sql
+++ AA_Questions/import_db.sql
@@ -1,5 +1,10 @@
 PRAGMA foreign_keys = ON;
-DROP TABLE users;
+
+DROP TABLE IF EXISTS question_follows;
+DROP TABLE IF EXISTS question_likes;
+DROP TABLE IF EXISTS replies;
+DROP TABLE IF EXISTS questions;
+DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
     id INTEGER PRIMARY KEY,
@@ -7,24 +12,35 @@ CREATE TABLE users (
     lname TEXT
 );
 
+-- DROP TABLE IF EXISTS questions;
+
 CREATE TABLE questions (
     id INTEGER PRIMARY KEY,
     title TEXT NOT NULL,
     body BLOB,
+    user_id INTEGER NOT NULL,
 
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
+-- DROP TABLE IF EXISTS question_follows;
+
 CREATE TABLE question_follows (
     id INTEGER PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    question_id INTEGER NOT NULL,
     
     FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (question_id) REFERENCES questions(id)
 );
 
+-- DROP TABLE IF EXISTS replies;
+
 CREATE TABLE replies (
     id INTEGER PRIMARY KEY,
     body BLOB NOT NULL,
+    user_id INTEGER NOT NULL,
+    question_id INTEGER NOT NULL,
     parent_reply_id INTEGER, 
 
     FOREIGN KEY (question_id) REFERENCES questions(id),
@@ -32,8 +48,12 @@ CREATE TABLE replies (
     FOREIGN KEY (parent_reply_id) REFERENCES replies(id)
 );
 
+-- DROP TABLE IF EXISTS question_likes;
+
 CREATE TABLE question_likes (
     id INTEGER PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    question_id INTEGER NOT NULL,
 
     FOREIGN KEY (question_id) REFERENCES questions(id),
     FOREIGN KEY (user_id) REFERENCES users(id)
@@ -53,3 +73,25 @@ INSERT INTO
 VALUES
     ('DSA', 'What is LSUCache??', (SELECT id FROM users WHERE fname = 'Swathi')),
     ('SQL', 'What is a correlated query?', (SELECT id FROM users WHERE fname = 'Davis'));
+
+INSERT INTO
+    question_follows (user_id, question_id)
+VALUES
+    (4, 1),
+    (3, 2);
+
+INSERT INTO
+    replies (body, user_id, question_id)
+VALUES
+    ('something abt dSA', 1, 1);
+
+INSERT INTO
+    replies (body, user_id, question_id, parent_reply_id)
+VALUES
+    ('something else abt dSA from paulo', 2, 1, 1);
+
+
+INSERT INTO
+    question_likes (user_id, question_id)
+VALUES
+    (1, 1);
